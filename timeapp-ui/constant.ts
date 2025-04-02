dba71244a357406c011151f622bb826fd7217f6f
Miracle Mayowa Adebunmi <36198643+miracle5284@users.noreply.github.com>
2025-04-02 02:35:54 +0100

    Add Nav bar with Sign in and Sign Up included

--- timeapp-ui/constant.ts
+++ timeapp-ui/constant.ts
@@ -1,2 +1,9 @@
 export const BACKEND_URL = import.meta.env.VITE_API_URL;
 export const CHROME_EXTENSION_PREFIX_URL = "https://chromewebstore.google.com/detail";
+
+export const navItems = [
+    {
+        href: "/countdown",
+        label: "Timer"
+    },
+]
--- timeapp-ui/src/App.tsx
+++ timeapp-ui/src/App.tsx
@@ -1,14 +1,19 @@
 import './index.css';
 import { Routes, Route } from "react-router-dom"
 import CountDown from "./pages/countdown";
+import Navbar from "./components/navbar.tsx";
 
 function App() {
     return (
-        <main className="min-h-screen flex flex-col items-center justify-center p-4 bg-[#222222]">
-            <Routes>
-                <Route path="/" element={<CountDown />} />
-            </Routes>
-        </main>
+        <>
+            <Navbar />
+            <main className="min-h-screen flex flex-col items-center justify-center p-4 bg-[#222222]">
+                <Routes>
+                    <Route path="/" element={<CountDown />} />
+                    <Route path="/countdown" element={<CountDown />} />
+                </Routes>
+            </main>
+        </>
     )
 }
 
