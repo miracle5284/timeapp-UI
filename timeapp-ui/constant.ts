f8629c5324c6959db10bce1b914ec775c15d066d
Miracle Mayowa Adebunmi <36198643+miracle5284@users.noreply.github.com>
2025-05-03 13:30:14 +0100

    Fix variable reference variable

--- timeapp-ui/constant.ts
+++ timeapp-ui/constant.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL = 'https://127.0.0.1:8005';
+export const BACKEND_URL = import.meta.env.VITE_API_URL;
 export const CHROME_EXTENSION_PREFIX_URL = "https://chromewebstore.google.com/detail";
 export const USER_DATA_ENDPOINT = `/users`;
 export const USER_AUTH_TOKEN_ENDPOINT = `/users/auth/token`;
